b7fba21e9d92c1693b0ed47b502be9898528b62d
nghiaphan04 <nghiaphan04dev@gmail.com>
2024-11-02 01:11:02 +0700

    responsive homepage

--- BTLWed/obj/Debug/net8.0/BTLWed.AssemblyInfo.cs
+++ BTLWed/obj/Debug/net8.0/BTLWed.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLWed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6669576991371a4f5dba7751884ec1460b2f8136")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLWed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLWed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
